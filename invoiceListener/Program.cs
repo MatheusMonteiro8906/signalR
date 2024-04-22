f03f275f326547230b5c125906a7c085fc331383
Matheus Monteiro <41351829+MatheusMonteiro8906@users.noreply.github.com>
2024-04-22 18:50:34 -0300

    first commit

---
+++ invoiceListener/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.AspNetCore.SignalR.Client;
+
+const string dateListenerURL = "http://localhost:5062/date";
+await using var connection = new HubConnectionBuilder()
+    .WithUrl(dateListenerURL)
+    .Build();
+
+await connection.StartAsync();
+
+await foreach (var date in connection
+    .StreamAsync<DateTime>("streaming"))
+{
+    Console.WriteLine(date);
+}
---
+++ invoiceReceiver/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.SignalR;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+app.MapHub<MyHub>("/date");
+
+app.MapGet("/", () => "my signalR project!");
+
+app.Run();
+
+class MyHub : Hub
+{
+    public async IAsyncEnumerable<DateTime> Streaming(
+        CancellationToken cancellationToken)
+    {
+        while (true)
+        {
+        yield return DateTime.UtcNow;
+        await Task.Delay(1000, cancellationToken);
+        }
+    }
+}
